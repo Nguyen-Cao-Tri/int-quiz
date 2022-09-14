8fab486c888f19ebebdb20e7cf45c527aa7067a4
Nguyen-Cao-Tri <nguyencaotri8137@gmail.com>
2022-09-14 12:11:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { useState, useEffect } from "react";
 import "./App.css";
-
+import datas from "./database/data.json";
 import Advisor from "./components/Advisor/Advisor";
 
 function App() {
   const [data, setData] = useState([]);
+  const new_data = datas.data;
   useEffect(() => {
-    fetch("http://localhost:8000/data")
-      .then((res) => {
-        return res.json();
-      })
-      .then((db) => {
-        db.advisorProfileCollection.items.map(item=>{
-          const random = Math.floor(Math.random() * 2)
-          if(random===0){
-            return item.status="online"
-          }
-          return item.status="offline"
-        })
-        setData(db.advisorProfileCollection.items);
-      });
+    new_data.advisorProfileCollection.items.map((item) => {
+      const random = Math.floor(Math.random() * 2);
+      if (random === 0) {
+        return (item.status = "online");
+      }
+      return (item.status = "offline");
+    });
+    setData(new_data.advisorProfileCollection.items);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return (data.length > 0 && <Advisor items={data} />);
+  return data.length > 0 && <Advisor items={data} />;
 }
 
 export default App;
